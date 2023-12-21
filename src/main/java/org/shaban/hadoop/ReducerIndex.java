e79ef08fb5a0c27bd275e3886e636c1321d6f9aa
Mohamed Shaban <ms2951@fayoum.edu.eg>
2023-12-21 21:59:07 +0200

    sorry, I forgot to push that file

--- src/main/java/org/shaban/hadoop/ReducerIndex.java
+++ src/main/java/org/shaban/hadoop/ReducerIndex.java
@@ -9,9 +9,14 @@ import java.util.Set;
 
 public class ReducerIndex extends Reducer<Text, Text, Text, Text> {
     private final Text result = new Text();
+
+    //create two documents for and operation
     Set<String> documentsOfFirstWord = new HashSet<>();
     Set<String> documentsOfSecondWord = new HashSet<>();
 
+    //create two documents for or operation
+    Set<String> firstDoc = new HashSet<>();
+    Set<String> secondDoc = new HashSet<>();
     String firstWord = "";
     String secondWord = "";
     private boolean firstIteration = true;
@@ -20,10 +25,12 @@ public class ReducerIndex extends Reducer<Text, Text, Text, Text> {
 
     @Override
     protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+
         if (firstIteration) {
             for (Text value : values) {
                 firstWord = key.toString();
                 documentsOfFirstWord.add(value.toString());
+                firstDoc.add(value.toString());
             }
 
             String wordWithDocs = key.toString() + " appears in: \t" + String.join(",", documentsOfFirstWord);
@@ -34,6 +41,7 @@ public class ReducerIndex extends Reducer<Text, Text, Text, Text> {
             for (Text value : values) {
                 secondWord = key.toString();
                 documentsOfSecondWord.add(value.toString());
+                secondDoc.add(value.toString());
             }
 
             String wordWithDocs = key.toString() + " appears in: \t" + String.join(",", documentsOfSecondWord);
@@ -45,26 +53,43 @@ public class ReducerIndex extends Reducer<Text, Text, Text, Text> {
 
     @Override
     protected void cleanup(Context context) throws IOException, InterruptedException {
+
         String word1 = context.getConfiguration().get(Constant.QUERY_WORD_1);
         String word2 = context.getConfiguration().get(Constant.QUERY_WORD_2);
 
         String andResult;
         String orResult;
 
+        //words appears
+        if (!word1.equals(firstWord) && !word1.equals(secondWord)) {
+            String word1Appears = word1 + " result is: \t" + " this word not appears in files \t";
+            context.write(new Text(word1Appears), result);
+        }
+        if (!word2.equals(firstWord) && !word2.equals(secondWord)) {
+            String word2Appears = word2 + " result is: \t" + " this word not appears in files \t";
+            context.write(new Text(word2Appears), result);
+        }
+
+        //and operation
         if (documentsOfSecondWord.isEmpty() || documentsOfFirstWord.isEmpty()) {
             andResult = word1 + " AND " + word2 + " result is: \t" + " There is no intersection between the two words ";
         } else {
             andResult = WordOperation.performAND(firstWord, secondWord, documentsOfFirstWord, documentsOfSecondWord);
+            //System.out.println(andResult.length());
+            if (andResult.length() == 26) {
+                andResult = word1 + " AND " + word2 + " result is: \t" + " There is no intersection between the two words ";
+
+            }
         }
         context.write(new Text(andResult), result);
-        if (documentsOfSecondWord.isEmpty() && documentsOfFirstWord.isEmpty()) {
+        //OR operation
+        if (secondDoc.isEmpty() && firstDoc.isEmpty()) {
             orResult = word1 + " OR " + word2 + " result is: \t" + "the two words not appears ";
-        } else if (documentsOfSecondWord.isEmpty()) {
-            orResult = word1 + " OR " + word2 + " result is: \t" + String.join(", ", documentsOfFirstWord);
-        } else if (documentsOfFirstWord.isEmpty()) {
-            orResult = word1 + " OR " + word2 + " result is: \t" + String.join(", ", documentsOfSecondWord);
+        } else if (secondDoc.isEmpty()) {
+            orResult = word1 + " OR " + word2 + " result is: \t" + String.join(", ", firstDoc);
+
         } else {
-            orResult = WordOperation.performOR(firstWord, secondWord, documentsOfFirstWord, documentsOfSecondWord);
+            orResult = WordOperation.performOR(firstWord, secondWord, firstDoc, secondDoc);
         }
         context.write(new Text(orResult), result);
     }
